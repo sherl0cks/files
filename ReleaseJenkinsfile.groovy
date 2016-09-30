b102344274d505e7b434d6bb5ad7033b11fc5162
Justin Holmes <justinmichaelholmes@gmail.com>
2016-09-30 10:32:18 -0600

    fix

---
+++ ReleaseJenkinsfile.groovy
@@ -0,0 +1,2 @@
+dynamicPipeline.withReleaseType().withHttpConfiguration('http://api-dev.apps.env2-1.etl.rht-labs.com/labs_env_demo.json').withApplicationName("infographic").generateAndExecutePipelineScript()
+
